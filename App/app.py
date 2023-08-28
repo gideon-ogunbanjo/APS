bc4f97e2da0711fb5680649c21741edb76dc8b63
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-28 10:51:55 +0100

    made minor changes

--- App/app.py
+++ App/app.py
@@ -44,7 +44,12 @@ def main():
 
         if model_choice == "Linear Regression":
             model = LinearRegression()
-            model_code = "from sklearn.linear_model import LinearRegression \n init: clf = LinearRegression() \n train: clf.fit(X_train, y_train) \n evaluate: score = mean_squared_error(y_test, clf.predict(X_test))"
+            model_code = """
+                from sklearn.linear_model import LinearRegression
+                clf = LinearRegression()
+                clf.fit(X_train, y_train)
+                score = mean_squared_error(y_test, clf.predict(X_test))
+            """
         elif model_choice == "Decision Tree":
             model = DecisionTreeRegressor()
             model_code = "model = DecisionTreeRegressor()\nmodel.fit(X_train, y_train)"
